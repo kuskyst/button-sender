309b3d494005800cc29f171b26cbd44db72f9a7b
kuskyst <126965999+kuskyst@users.noreply.github.com>
2024-09-23 00:11:41 +0900

    Update content.js

--- content.js
+++ content.js
@@ -1,16 +1,17 @@
 document.addEventListener('click', function(event) {
-  alert(event.target.textContent);
 
-  const scriptUrl = 'https://script.google.com/macros/s/AKfycbxSSCSTA50TE7k_VgO_r-VJr768tL6zokLViSJNYzuMbFSV5_FAN43Uls6cYw5kk6Uw2A/exec?action=greet';
+  if (event.target.textContent.includes('審査に送信')) {
+    const scriptUrl = 'https://script.google.com/macros/s/xxxx';
 
-  fetch(scriptUrl)
-  .then(response => response.text())
-  .then(data => {
-    console.log(data);
-    alert(data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+    fetch(scriptUrl)
+    .then(response => response.text())
+    .then(data => {
+      console.log(data);
+      alert(data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  }
 
 });
